c20ba1cf3764ee0255213d763d8099d8b03b3531
Jesus Villamarin <jesus.villamarin@getpulpo.com>
2023-07-21 17:23:38 -0500

    fix: 🐛 Remove console log

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,9 +8,7 @@ export const dynamic = 'force-dynamic';
 
 export default async function Home({ searchParams }: PropsParams) {
     const { key, value } = Object.entries(searchParams).reduce((_, [key, value]) => ({ key, value }), {}) as { key: string; value: string };
-
     const countries: Country[] = key && value ? await filterCountryBy(key, value as string) : await getAllCountries();
-    console.log(countries);
 
     return (
         <main className='main-container'>
