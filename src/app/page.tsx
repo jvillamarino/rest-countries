7ec081c006dde5e972ee4bab174d406e410b547e
Jesus Villamarin <jesus.villamarin@getpulpo.com>
2023-07-21 17:16:42 -0500

    fix: 🐛 Rerendered on filter

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,13 @@ import { getAllCountries, filterCountryBy } from '../api/country.api';
 import { FilterComponent } from '@/components/filter';
 import { PropsParams } from '@/models';
 
+export const dynamic = 'force-dynamic';
+
 export default async function Home({ searchParams }: PropsParams) {
     const { key, value } = Object.entries(searchParams).reduce((_, [key, value]) => ({ key, value }), {}) as { key: string; value: string };
 
     const countries: Country[] = key && value ? await filterCountryBy(key, value as string) : await getAllCountries();
+    console.log(countries);
 
     return (
         <main className='main-container'>
